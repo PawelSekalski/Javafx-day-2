c1440bc3f1a71521c3378a5989667bda41b53db4
PS <pg.zap34@gmail.com>
2017-09-10 23:31:09 +0200

    first commit

---
+++ src/app/Main.java
@@ -0,0 +1,23 @@
+package app;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage Stage) throws Exception {
+		Parent parent = (Parent) FXMLLoader.load(getClass().getResource("/app/view/MainView.fxml"));
+		Scene scene = new Scene (parent);
+		Stage.setScene(scene);
+		Stage.setTitle("Second JavaFX app");
+		Stage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
